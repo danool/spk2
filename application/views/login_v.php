99ef70f6f4ba7e82712fa4a56fb9ad78889624cc
febriyanyoga <febriyanyoga@gmail.com>
2017-06-09 15:25:31 +0700

    1.1

--- application/views/login_v.php
+++ application/views/login_v.php
@@ -1,35 +1,4 @@
-<!-- <div>	
-	 <h1>Login</h1>	
-	 <?php #echo form_open(base_url().'login/')	?>	
-	 <?php #echo validation_errors(); ?>	
-	 <span><b><?php #echo $login_failed; ?></b></span>	
-	 <div>
-	 	 <div>Username:	</div>	
-	 	 <div><input type="text" name="username" value="<?php #echo set_value('username');?>"/></div>	
-	 </div>	
-	 <div>	
-	 	 <div>Password:	</div>	
-	 	 <div><input type="password" name="password" value="<?php #echo set_value('password');?>"/><br/></div>	
-	 </div>	
-	 <div>	
-	 	 <input	type="submit" value="Login" name="submit_login"/>	
-	 </div>
-	 <div>	
-	 	 Apakah Anda belum register? <?php #echo anchor('register', 'Register');?>	
-	 </div>
-	 <?php #echo form_close()?>	
-</div> --><!-- 
-<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" >
-	<div class="modal-dialog" role="document">
-		<div class="modal-content modal-info">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-			</div>
-			<div class="modal-body modal-spa">
-				
-		</div>
-	</div>
-</div> -->
+
 <div class="container">
 	<div class="row">
 		<div class="col-md-3 pull-left">
@@ -65,9 +34,7 @@
 				</div>
 			</div>
 		</div>
-		<!-- <div class="col-md-5 pull-right">
-		<p> ini rata kanan</p>
-		</div> -->
+	
 
 
 	</div>
